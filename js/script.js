cd8cba32fa10134984d52d0b9e980bff74338e63
UdayGanti <udayganti22@gmail.com>
2023-04-22 19:01:00 -0400

    Updated Tabs with working JS

--- js/script.js
+++ js/script.js
@@ -16,29 +16,5 @@ setInterval(() => {
   index = (index + 1) % images.length;
 }, 2000); // 2 seconds
 
-
 changeBackground(); // Call the function once to set the initial background image
-
 setInterval(changeBackground, 2000); // Call the function every 2 seconds to change the background image
-
-///Bootstraping JS///
-
-const tabs = document.querySelector(".tabs");
-const btns = document.querySelectorAll(".button");
-const articles = document.querySelectorAll(".content");
-tabs.addEventListener("click", function (e) {
-  const id = e.target.dataset.id;
-  if (id) {
-    // remove selected from other buttons
-    btns.forEach(function (btn) {
-      btn.classList.remove("live");
-    });
-    e.target.classList.add("live");
-    // hide other articles
-    articles.forEach(function (article) {
-      article.classList.remove("live");
-    });
-    const element = document.getElementById(id);
-    element.classList.add("live");
-  }
-});
